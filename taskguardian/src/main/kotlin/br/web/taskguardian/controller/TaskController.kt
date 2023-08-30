2da42d29e9c869c80152506d8c17d798435bc91c
leocunha2030 <leocunha20030@hotmail.com>
2023-08-30 20:39:44 -0300

    Task list added

--- taskguardian/src/main/kotlin/br/web/taskguardian/controller/TaskController.kt
+++ taskguardian/src/main/kotlin/br/web/taskguardian/controller/TaskController.kt
@@ -2,26 +2,32 @@ package br.web.taskguardian.controller
 
 import br.web.taskguardian.controller.request.PostTaskController
 import model.TaskModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("tasks")
 class TaskController {
 
+    val tasks = mutableListOf<TaskModel>()
+
     @GetMapping
-    fun getTask(): TaskModel{
-        return TaskModel(
-            "1",
-            "Tarefa de fazer coisas",
-            "Essa tarefa é sobre fazer tal coisas"
-        )
+    fun getTask(): MutableList<TaskModel> {
+        return tasks
     }
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody task: PostTaskController) {
-        println(task)
+        var id = if (tasks.isEmpty()){
+            "1"
+        }else{
+            tasks.last().id.toInt() + 1
+        }.toString()
+        tasks.add(TaskModel(id,task.name,task.description))
     }
 }
